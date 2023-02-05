759a7348d734c30e44cd8f6e74829075dbcf1773
Ricardo <ricardinhogiasson16@gmail.com>
2023-02-04 23:07:37 -0300

    Connectando e Criando Primeiras Filas Rabbit MQ

--- src/main/java/com/estoque/EstoqueApplication.java
+++ src/main/java/com/estoque/EstoqueApplication.java
@@ -1,12 +1,22 @@
 package com.estoque;
 
 import org.springframework.boot.SpringApplication;
+import connections.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rabbitmq.client.ConnectionFactory;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 @SpringBootApplication
 public class EstoqueApplication {
 
 	public static void main(String[] args) {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost", 5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+		RabbitMQConnection conn = new RabbitMQConnection(new RabbitAdmin(connectionFactory));
 		SpringApplication.run(EstoqueApplication.class, args);
 	}
 
